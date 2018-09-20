2762a3cd9db5c0089cf93a431df4a888370fcfa3
t00202160 <paul.cafferkey@students.ittralee.ie>
2018-09-20 15:56:35 +0100

    bb

--- src/MathProblem.java
+++ src/MathProblem.java
@@ -4,6 +4,16 @@ public class MathProblem {
     }
 
     public static void doCalc(){
-        
+        float a = 1;
+        float b = 6;
+        float c = -16;
+
+        float ans,ans2 ;
+
+      ans=(float) (-(b)-(Math.sqrt(Math.pow(b,2))-(4*(a*c))))/2*a ;
+      ans2=(float) (-(b)+(Math.sqrt(Math.pow(b,2))-(4*(a*c))))/2*a ;
+
+        System.out.println(ans);
+        System.out.println(ans2);
     }
 }
---
+++ src/cube.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class cube {
+    public static void main(String[] args) {
+        doCalc();
+    }
+
+    public static void doCalc(){
+
+
+
+        for(int i=1;i<16;i++){
+
+            double ans = Math.pow(i,3);
+
+        }
+    }
+}
